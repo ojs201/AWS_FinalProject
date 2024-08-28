419a38c7c79ca42a83c5f47bc3b3a96e70979e28
Omega <jaesukoh@naver.com>
2024-08-28 12:30:58 +0900

    test

--- main_page/scripts.js
+++ main_page/scripts.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // 예시: 'User Data' 버튼 클릭 시 알림창 표시
     document.querySelector('.btn').addEventListener('click', () => {
         alert('User Data 버튼이 클릭되었습니다.');
     });
