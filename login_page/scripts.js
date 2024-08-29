820c154d241e96a9a58709892c5600ded9f520b5
rttitity <jinwoo25803@gmail.com>
2024-08-29 16:05:32 +0900

    덮어쓰기: main 브랜치의 파일로 덮어쓰기

--- login_page/scripts.js
+++ login_page/scripts.js
@@ -1,3 +1,4 @@
+// 현재 이 파일은 비어있지만, JavaScript를 사용해 향후 인터랙션을 추가할 수 있습니다.
 document.addEventListener('DOMContentLoaded', () => {
     // 예시: 'User Data' 버튼 클릭 시 알림창 표시
     document.querySelector('.btn').addEventListener('click', () => {
