fec956f36db0507b35b260bf4334f1a257be578f
rttitity <jinwoo25803@gmail.com>
2024-09-03 00:22:17 +0900

    upload image to S3

--- main_page/scripts.js
+++ main_page/scripts.js
@@ -69,7 +69,7 @@ function uploadProfileImage() {
             return alert("Please choose a file to upload first.");
         }
         var file = files[0];
-        var fileName = `${email}/profile/${file.name}`;  // 이메일을 경로의 일부로 사용
+        var fileName = `User_data/${email}/profile/${file.name}`;  // 이메일을 경로의 일부로 사용
 
         var upload = new AWS.S3.ManagedUpload({
             params: {
@@ -110,7 +110,7 @@ document.addEventListener("DOMContentLoaded", function() {
         ClientId: '30ru3mhl5fgmsm9ll9h7kmh2p5', // 클라이언트 ID
     };
     
-    const logoutButton = document.querySelector('.profile_btn');
+    const logoutButton = document.querySelector('.logout_btn');
 
     if (logoutButton) {
         logoutButton.addEventListener("click", function() {
